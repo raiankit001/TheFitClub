4f65b0e35afe8c6582652e6a8f49581d248ef574
Ankit Rai <rv.raiankit001@gmail.com>
2024-01-19 00:55:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Join from "./components/Join/Join";
+import LandingPage from "./components/LandingPage/LandingPage";
+import Plans from "./components/Plans/Plans";
+import Programs from "./components/Programs/Programs";
+import Reasons from "./components/Reasons/Reasons";
+import ShopMini from "./components/ShopMini/ShopMini";
+import Testimonials from "./components/Testimonials/Testimonials";
+import {Link} from 'react-scroll'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <LandingPage />
+        <Programs />
+        <Reasons />
+        <ShopMini/>
+        <Plans/>
+        <Testimonials/>
+        <Join/>
+        <Footer/>
+     
+        {/* scroll to top btn */}
+        <div>
+          <Link 
+                className="topShortcut"
+                to="landingPage"
+                spy="true"
+                smooth="true"
+                ><i class="fa-solid fa-arrow-up"></i></Link>
+        </div>
+        
+      </div>
+    </>
   );
 }
 
